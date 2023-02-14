b197398164484e1e4dfadfde342fa448b874cab8
Su Shruthi <shruthacm@gmail.com>
2023-02-14 11:35:31 -0800

    added functionality to stop the game when any one player reaches 100 and is the winner of the game

--- script.js
+++ script.js
@@ -19,6 +19,14 @@ const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generating a random dice roll
@@ -37,10 +45,28 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
+  }
+});
+// player holding a current score
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active player's score
+  score[activePlayer] += currentScore;
+  //scores[1] = scores[1] + currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    score[activePlayer];
+
+  // 2. Check if player's score is >= 100
+  if (score[activePlayer] >= 100) {
+    // Finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add(`player--winner`);
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove(`player--active`);
+  } else {
+    // Switch to the next player
+    switchPlayer();
   }
 });
